04634ba9f45d662e6c73aee7c2b0e4a83ea98894
Aida Papic <northantsvintage@gmail.com>
2018-04-02 14:16:51 +0100

    validated js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-// After following this tutorial, manage to finish the game
-// cardFlip function
+// Following various tutorials on Memory Game
+// cardFlip function from Adam Khoury Youtube Tutorial
 // http://www.youtube.com/watch?v=c_ohDPWmsM0
-const cardsArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+let cardsArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
 let cardsStorage = [];
 let cardsIds = [];
 let cardsFlipped = 0;
 
-let countMoves = document.querySelector(".moves");
-const stars = document.querySelectorAll(".fa-star"); // identify variables for star icons
+let countMoves = document.querySelector('.moves');
+const stars = document.querySelectorAll('.fa-star'); // identify variables for star icons
 let deck = document.getElementById('deck');
+const restartBtn = document.querySelector('.restart');
 // Get the modal
 let modal = document.getElementById('myModal');
 const modalContent = document.querySelector('.modal-content p');
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName('close')[0];
 let modalMessage = '';
 
 // time variables
-let second = 0,
-    minute = 0,
-    hour = 0;
-let timer = document.querySelector(".timer");
+let second = 0;
+let minute = 0;
+let hour = 0;
+let timer = document.querySelector('.timer');
 let interval;
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length,
-        temporaryValue, randomIndex;
+    let currentIndex = array.length,temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -46,15 +46,15 @@ function newBoard() {
     cardsFlipped = 0;
     let displayCards = '';
     shuffle(cardsArray); // shuffle cards
-    for (let i = 0; i < cardsArray.length; i++) {
-        displayCards += '<div class="card" id="card_' + i + '" onclick="cardFlip(this,\'' + cardsArray[i] + '\')"></div>';
+    for (i = 0; i < cardsArray.length; i++) {
+        displayCards += '<div class="card" id="card_'+i+'" onclick="cardFlip(this,\''+cardsArray[i]+'\')"></div>';
     }
 
     deck.innerHTML = displayCards;
     // reset stars rating
     for (let i = 0; i < stars.length; i++) {
-        stars[i].style.color = "#FFD700";
-        stars[i].style.visibility = "visible";
+        stars[i].style.color = '#ffd700';
+        stars[i].style.visibility = 'visible';
     }
 
 }
@@ -62,9 +62,9 @@ function newBoard() {
 newBoard();
 
 function cardFlip(card, val) {
-    if (card.innerHTML == "" && cardsStorage.length < 2) {
+    if (card.innerHTML == '' && cardsStorage.length < 2) {
         moveCounter();
-        card.style.background = '#FFF';
+        card.style.background = '#ffffff';
         card.innerHTML = val;
         if (cardsStorage.length == 0) {
             // gets fitst card [0] and pushes into array
@@ -79,14 +79,13 @@ function cardFlip(card, val) {
             if (cardsStorage[0] == cardsStorage[1]) {
                 // counts 8 pairs
                 cardsFlipped += 2;
-
                 // Clear both arrays
                 cardsStorage = [];
                 cardsIds = [];
                 // Check to see if the whole board is cleared
                 if (cardsFlipped === cardsArray.length) {
                     // modal starts here
-                    modal.style.display = "block";
+                    modal.style.display = 'block';
                     modalMessage = 'Congratulations! You are the winner!'
                     modalMessage += '<br>Time: ' + timer.innerText;
                     modalMessage += '<br>It took you ' + countMoves.innerText + ' moves';
@@ -95,10 +94,10 @@ function cardFlip(card, val) {
                     modalContent.innerHTML = modalMessage;
 
                     span.addEventListener('click', function() {
-                      modal.style.display = "none";
+                      modal.style.display = 'none';
                     });
                     resetBoard();
-                    deck.innerHTML = "";
+                    deck.innerHTML = '';
                     newBoard();
                 }
             } else {
@@ -108,12 +107,12 @@ function cardFlip(card, val) {
                     let cardTwo = document.getElementById(cardsIds[1]);
 
                     cardOne.style.background = 'linear-gradient(160deg, #f4ae99 0%, #bb7ebd 100%)';
-                    cardOne.innerHTML = "";
+                    cardOne.innerHTML = '';
                     // add style.animation here
                     cardTwo.style.background = 'linear-gradient(160deg, #f4ae99 0%, #bb7ebd 100%)';
-                    cardTwo.innerHTML = "";
+                    cardTwo.innerHTML = '';
                     // add style.animation here
-                    
+
                     // Clear both arrays
                     cardsStorage = [];
                     cardsIds = [];
@@ -129,7 +128,7 @@ function cardFlip(card, val) {
 // Function to Calculate Game Time
 function startTimer() {
     interval = setInterval(function() {
-        timer.innerHTML = minute + " mins " + second + " secs ";
+        timer.innerHTML = minute + ' mins ' + second + ' secs ';
         second++;
         if (second == 60) {
             minute++;
@@ -156,13 +155,13 @@ function moveCounter() {
     if (moves > 16 && moves < 28) {
         for (i = 0; i < 3; i++) {
             if (i > 1) {
-                stars[i].style.visibility = "collapse";
+                stars[i].style.visibility = 'collapse';
             }
         }
     } else if (moves > 24) {
         for (i = 0; i < 3; i++) {
             if (i > 0) {
-                stars[i].style.visibility = "collapse";
+                stars[i].style.visibility = 'collapse';
             }
         }
     }
@@ -175,3 +174,8 @@ function resetBoard() {
     countMoves.innerHTML = '';
     newBoard();
 }
+
+//Event Listener to restart game
+restartBtn.addEventListener('click', function() {
+  resetBoard();
+});
